235ded29951085e0fa5606afdc1b5573ac70e253
Daniel Choi <dhchoi@gmail.com>
2014-09-13 11:07:44 -0400

    minor

--- Main.hs
+++ Main.hs
@@ -6,5 +6,6 @@ import qualified Data.ByteString.Char8 as B
 
 main = do
   input <- B.getContents
+  print $ extractPath input -- useless
   print $ parseQueryText input
   print $ decodePathSegments input
